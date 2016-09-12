1e95f8d246843da6fd01cc0301be220bbfdf043d
ThisuraThejith <ttcphilips@gmail.com>
2016-09-13 01:47:11 +0530

    modified set attendance

--- src/org/itp/commons/Queries.java
+++ src/org/itp/commons/Queries.java
@@ -41,6 +41,7 @@ public class Queries {
             public static String SEARCH_EMPLOYEE="select * from Employee where First_Name like ? or Last_Name like ? or Address like ? or Date_of_Birth like ? or NIC_No like ? or Gender like ? or Marital_Status like ? or Mobile_No like ? or Home_No like ? or Email like ?or Current_Status like ?or RoleID like ?";
             public static String SEARCH_ATTENDANCE_TOGETHER="select EmpID,First_Name,Last_Name,Work_Date,Presence from Employee e,Attendance a where e.EmployeeID=a.EmpID and (Work_Date=? and (EmpID like ? or First_Name like ? or Last_Name like ?))";
             public static String SEARCH_ATTENDANCE_SEPERATE="select EmpID,First_Name,Last_Name,Work_Date,Presence from Employee e,Attendance a where e.EmployeeID=a.EmpID and (Work_Date=? or (EmpID like ? or First_Name like ? or Last_Name like ?))";
+            public static String SEARCH_ATTENDANCE_SEPERATE_KEY="select EmpID,First_Name,Last_Name,Work_Date,Presence from Employee e,Attendance a where e.EmployeeID=a.EmpID and (EmpID like ? or First_Name like ? or Last_Name like ?)";
         }
         public static class Update{
             public static String EMPLOYEE="Update Employee Set First_Name=?,Last_Name=?,Address=?,Date_of_Birth=?,NIC_No=?,Gender=?,Marital_Status=?,Mobile_No=?,Home_No=?,Email=?,Current_Status=?,RoleID=? Where EmployeeID=?";
---
+++ src/org/itp/openems/model/Attendance.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.itp.openems.model;
+
+/**
+ *
+ * @author THISURA THEJITH
+ */
+public class Attendance {
+    private String workDate;
+    private int employeeID;
+    private String firstName;
+    private String lastName;
+    private String presence;
+
+    public String getPresence() {
+        return presence;
+    }
+
+    public void setPresence(String presence) {
+        this.presence = presence;
+    }
+
+    public String getWorkDate() {
+        return workDate;
+    }
+
+    public void setWorkDate(String workDate) {
+        this.workDate = workDate;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+}
